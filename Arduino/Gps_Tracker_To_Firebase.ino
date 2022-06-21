3fe25ca448c1aae6c1330b1f5d437d4933ff0396
Emil766b <you@example.com>
2022-06-21 18:44:44 +0200

    Arduino code comments

--- Arduino/Gps_Tracker_To_Firebase.ino
+++ Arduino/Gps_Tracker_To_Firebase.ino
@@ -3,12 +3,14 @@
 
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
-#define FIREBASE_HOST""
-#define FIREBASE_AUTH""
+#define FIREBASE_HOST ""
+#define FIREBASE_AUTH ""
 
+// Firebase dataobject
 FirebaseData firebaseData;
-// path based on date
+// Firebase path
 String path ="/LocationData";
+// Use json
 String jsonStr;
 
 void setup() {
@@ -18,26 +20,34 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  // If you are using the MKR GPS as shield, change the next line to pass
-  // the GPS_MODE_SHIELD parameter to the GPS.begin(...)
+  // If GPS does not begin print error
+
   if (!GPS.begin()) {
     Serial.println("Failed to initialize GPS!");
     while (1);
   }
 
-    Serial.println("Connection to WIFI");
+  // Print to console
+  Serial.println("Connecting to WIFI");
+  // Get WiFi status
   int status = WL_IDLE_STATUS;
 
+  // While WiFi is not connected
   while(status != WL_CONNECTED)
   {
+    // Connect to WiFi
     status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    // Print to console
     Serial.print("Tracking down WiFi\n");
     delay(500);
   }
+  // Print Ip address to console
   Serial.print(" Forbundet til ip:");
   Serial.println(WiFi.localIP());
-
+  
+  // Begin firebase connection
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH, WIFI_SSID, WIFI_PASSWORD);
+  // Reconnect WiFi if lost
   Firebase.reconnectWiFi(true);
   }
 
@@ -51,8 +61,9 @@ void loop() {
     float altitude   = GPS.altitude();
     float speed      = GPS.speed();
     int   satellites = GPS.satellites();
-
     
+    // SetFloat of path and GPS values
+    // Print data path and GPS values
     if (Firebase.setFloat(firebaseData, path + "/Latitude", latitude)) {
       Serial.println(firebaseData.dataPath() + " = " + latitude);
     }
@@ -68,16 +79,19 @@ void loop() {
     if (Firebase.setFloat(firebaseData, path + "/Satellites", satellites)) {
       Serial.println(firebaseData.dataPath() + " = " + satellites);
     }
+    // Format GPS data as json
     jsonStr = "{\"Latitude\":" + String(latitude, 7) + 
               ",\"Longitude\":" + String(longitude, 7) +
               ",\"Altitude\":" + String(altitude, 2) +
               ",\"Speed\":" + String(speed, 2) +
               ",\"Satellites\":" + String(satellites) +"}";
-
+    // Push json formatted data to PastData in firebase
     if (Firebase.pushJSON(firebaseData, path + "/PastData", jsonStr)) {
+      // Print firebase dataPath and pushName
       Serial.println(firebaseData.dataPath() + " = " + firebaseData.pushName());
     }
     else {
+      // Print error
       Serial.println("Error: " + firebaseData.errorReason());
     }
 
